54d446fd82f26c629b1f675019287b1ffa7f48aa
Kaleab Mengesha <mengeshakaleab99@gmail.com>
2025-07-13 04:47:17 -0800

    Fix import and usage of WelcomeMessage component in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,10 @@
-import UserProfile from './components/UserProfile';
-
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
-    <>
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <WelcomeMessage />
+    </div>
   );
 }
 
