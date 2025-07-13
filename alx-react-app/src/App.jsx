c5538ac3894e9714bf34f86ce09336e3917fe490
Kaleab Mengesha <mengeshakaleab99@gmail.com>
2025-07-13 05:36:58 -0800

    Update App.jsx to include WelcomeMessage along with other components for tasks 1, 2, and 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
 import Header from './components/Header';
@@ -5,18 +6,10 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
 function App() {
-  return (function App() {
   return (
     <div>
+      <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
-
-    <div>
       <Header />
       <MainContent />
       <Footer />
