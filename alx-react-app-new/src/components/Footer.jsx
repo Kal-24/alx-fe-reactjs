e78c68843b986e6604533870893f76cc72a8e26c
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-21 11:49:09 -0800

    Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '15px' }}>
+      <p>&copy; 2025 My Travel Blog. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
