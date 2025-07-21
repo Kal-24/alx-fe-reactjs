9380b73cbfd492d061c182a9bb881b124022b78b
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-21 14:04:27 -0800

    recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,5 +1,29 @@
-
 import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+/*import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
@@ -10,4 +34,4 @@ const useRecipeStore = create((set) => ({
   setRecipes: (recipes) => set({ recipes }),
 }));
 
-export { useRecipeStore };
+export { useRecipeStore };*/
