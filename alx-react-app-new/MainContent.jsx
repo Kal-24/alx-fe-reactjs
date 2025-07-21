6518678afb281eb5339f64e47a777e098a834dba
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-21 10:50:09 -0800

    MainContent.jsx

---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f0f0', fontSize: '18px', color: '#333' }}>
+      <p>Here is some main content about my favorite cities and why I love them!</p>
+    </main>
+  );
+};
+
+export default MainContent;
