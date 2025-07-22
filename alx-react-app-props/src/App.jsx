c0aa1a2c99c6e1c90393d8d5abb21c88125e96b8
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-21 22:58:26 -0800

    App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,34 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
-import EditRecipeForm from './components/EditRecipeForm';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
-import SearchBar from './components/SearchBar';
+// src/App.jsx
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <Router>
-      <div>
-        <h1>Recipe Sharing App</h1>
-
-        {/* Search and Other Components */}
-        <SearchBar />
-        <FavoritesList />
-        <RecommendationsList />
-
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/add" element={<AddRecipeForm />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-          <Route path="/edit/:id" element={<EditRecipeForm />} />
-        </Routes>
-      </div>
-    </Router>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
