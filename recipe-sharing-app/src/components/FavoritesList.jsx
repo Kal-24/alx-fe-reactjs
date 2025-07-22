f74fcc36ac620169105fef0905878169c1fdbcc0
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-21 21:58:24 -0800

    FavoritesList.jsx

---
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
+
+const FavoritesList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+  const favoriteIds = useRecipeStore(state => state.favorites);
+
+  const favorites = favoriteIds
+    .map(id => recipes.find(recipe => recipe.id === id))
+    .filter(Boolean);
+
+  return (
+    <div>
+      <h2>My Favorites</h2>
+      {favorites.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FavoritesList;
