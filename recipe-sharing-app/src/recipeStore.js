ba4e60638976e9d2d1fd3ba15446bf5befddf219
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-21 22:49:10 -0800

    recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -3,19 +3,27 @@ import create from 'zustand';
 const useRecipeStore = create((set, get) => ({
   recipes: [],
   favorites: [],
+  recommendations: [],
   searchTerm: '',
-  setSearchTerm: (term) => {
-    set({ searchTerm: term });
-    get().filterRecipes();
-  },
   filteredRecipes: [],
-  filterRecipes: () => {
-    const { recipes, searchTerm } = get();
-    const filtered = recipes.filter(recipe =>
-      recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    set({ filteredRecipes: filtered });
-  },
-}));
 
-export default useRecipeStore;
+  // Task 0: Set all recipes
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Task 1: Add, update, delete recipes
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  })),
+
+  deleteRecipe: (id) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
+
+  // Task 2: Search/filter recipes
+  setSearchTerm:
