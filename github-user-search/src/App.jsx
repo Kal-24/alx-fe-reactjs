3b5c319bd1ced8ebf7b51fa74bf0f471996b3282
Kal-24 <kaleabmengesha060@gmail.com>
2025-07-30 14:38:01 +0300

    App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -34,4 +34,17 @@ function App() {
   );
 }
 
+export default App; import React from 'react';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
 export default App;
+
