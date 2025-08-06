4f58632b3c3bc02c54987985ace3381e0a3ac67c
Kal-24 <kaleabmengesha060@gmail.com>
2025-08-06 16:44:33 +0300

    vite.config.ts

---
+++ tailwind-react-integration/src/vite.config.ts
@@ -0,0 +1,12 @@
+// vite.config.ts
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ Tailwind Vite plugin
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // ✅ Add Tailwind plugin here
+  ],
+})
