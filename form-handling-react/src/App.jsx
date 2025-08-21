e7bea02cda27002b707db0388fe754300988c406
Kal-24 <kaleabmengesha060@gmail.com>
2025-08-21 09:05:09 +0300

    App.jsx

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <RegistrationForm />
+      <hr />
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
