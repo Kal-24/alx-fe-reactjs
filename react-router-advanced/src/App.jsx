ffca1d976ef00e9ba0ada5efc7c25e7ebf740be6
Kal-24 <kaleabmengesha060@gmail.com>
2025-08-21 12:51:07 +0300

    App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Login from './components/Login';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ProfileDetails from "./pages/ProfileDetails";
+import ProfileSettings from "./pages/ProfileSettings";
+import Post from "./pages/Post";
+import BlogPost from "./pages/BlogPost"; // ✅ Import this
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
-    <Router>
+    <>
+      <nav>
+        <Link to="/">Home</Link> |
+        <Link to="/profile">Profile</Link> |
+        <Link to="/post/123">Post 123</Link> |
+        <Link to="/blog/abc">Blog abc</Link> {/* ✅ Add this link */}
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
 
-        {/* Protected Route Example */}
-        <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        <Route path="/post/:postId" element={<Post />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* ✅ Add this route */}
       </Routes>
-    </Router>
+    </>
   );
 }
 
 export default App;
-<Route path="/user/:userId" element={<UserProfile />} />
