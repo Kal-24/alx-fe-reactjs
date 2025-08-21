0bb9d3d7223f6405d88412410952c04d29261244
Kal-24 <kaleabmengesha060@gmail.com>
2025-08-21 09:18:09 +0300

    App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -31,5 +31,15 @@ export default function App() {
       <PostsComponent />
     </div>
   );
+}import React from 'react';
+import PostsComponent from './components/PostsComponent';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <PostsComponent />
+    </div>
+  );
 }
+
 
