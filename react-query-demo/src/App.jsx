cc9b465f665db40227c7fedcb2486ad96ac9bac0
Kal-24 <kaleabmengesha060@gmail.com>
2025-08-21 11:45:12 +0300

    App.jsx

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PostsComponent from './components/PostsComponent';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <PostsComponent />
+    </div>
+  );
+}
