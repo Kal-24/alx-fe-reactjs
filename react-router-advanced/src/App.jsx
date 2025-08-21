6ab76ba130c6c221cf4fde01a22b7a849c7de007
Kal-24 <kaleabmengesha060@gmail.com>
2025-08-21 12:04:19 +0300

    App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Route Example */}
+        <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
